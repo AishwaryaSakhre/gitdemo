90776ffd94de0106b2ddcacd0f6e8cd4114ad5c7
OM <OM@DESKTOP-44QF6Q5>
2024-06-20 21:37:06 +0530

    First comment.....

---
+++ src/gitdemo/demogit.java
@@ -0,0 +1,15 @@
+package gitdemo;
+
+public class demogit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry = "yes";
+		if(hungry == "yes")
+			System.out.Println("eat somthin...");
+		else {
+			System.out.Println("do work...");
+		}
+	}
+
+}
